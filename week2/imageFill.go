907c4b7b5fe51ed9301d3a33a300ae783a325557
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-05-11 19:21:27 +0100

    Small optimizations

--- week2/imageFill.go
+++ week2/imageFill.go
@@ -1,5 +1,6 @@
 package main;
 
+var m,n int
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
 	previousColour:=  image[sr][sc];
 
@@ -7,26 +8,25 @@ func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
 		return image;
 	}
 
+	m = len(image);
+	n = len(image[0])
+
 	fill(image, sr, sc, newColor, previousColour);
 	return image;
 }
 
 func fill(image [][]int, x int, y int, newColor int, previousColour int){
 
-	if x < 0 || x >= len(image) || y < 0 || y >=len(image[0]){
+	if x < 0 || x >= m || y < 0 || y >=n || image[x][y] != previousColour{
 		return;
 	}
 
-	if image[x][y] != previousColour{
-		return;
-	}else{
-		image[x][y] = newColor;
+	image[x][y] = newColor;
 
-		fill(image, x + 1, y, newColor, previousColour) // Bottom;
-		fill(image, x - 1, y, newColor, previousColour) // Top;
-		fill(image, x, y + 1, newColor, previousColour) // right;
-		fill(image, x, y - 1, newColor, previousColour) // Left;
-	}
+	fill(image, x + 1, y, newColor, previousColour) // Bottom;
+	fill(image, x - 1, y, newColor, previousColour) // Top;
+	fill(image, x, y + 1, newColor, previousColour) // right;
+	fill(image, x, y - 1, newColor, previousColour) // Left;
 }
 
 
