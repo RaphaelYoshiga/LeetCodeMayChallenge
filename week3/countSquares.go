19704aa18e5456171cde4bd55ddd3f52db9e8411
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-05-21 20:08:04 +0100

    Count squares using dynamic programming

---
+++ week3/countSquares.go
@@ -0,0 +1,28 @@
+package main;
+
+func countSquares(matrix [][]int) int {
+
+	sum := 0;
+	for i, row := range matrix{
+		for y, cell := range row{
+			if cell == 1{
+				value := cell;
+				if i > 0 && y > 0{
+					value += min(matrix[i-1][y], min(matrix[i-1][y-1], row[y-1]))
+				}
+
+				sum := value;
+				row[y] = value;
+			}
+		}
+	}
+
+	return sum;
+}
+
+func min(a int, b int) int{
+	if a > b{
+		return b;
+	}
+	return a;
+}
--- week3/smallesNodeBinaryTree.go
+++ week3/smallesNodeBinaryTree.go
@@ -8,42 +8,25 @@ package main;
  *     Right *TreeNode
  * }
  */
+ func bstSmallest(t *TreeNode, k, res *int){
+    if t == nil || *k == 0 {
+        return
+    }
+    
+    bstSmallest(t.Left, k, res)
+    
+    (*k)--
+    
+    if *k == 0 {
+        *res = t.Val
+        return
+    }
+
+    bstSmallest(t.Right, k, res)
+}
+
 func kthSmallest(root *TreeNode, k int) int {
-	min := root.Val;
-
-	bst(root)
-
-	return min;
-}
-
-const (
-    MaxInt  = 1<<(UintSize-1) - 1 // 1<<31 - 1 or 1<<63 - 1
-    MinInt  = -MaxInt - 1         // -1 << 31 or -1 << 63
-    MaxUint = 1<<UintSize - 1     // 1<<32 - 1 or 1<<64 - 1
-)
-
-func bst(node *TreeNode) int{
-	if node == nil {
-		return MaxInt;
-	}
-
-	right := bst(node.Right);
-	left := bst(node.Left);
-
-	childMin := min(left, right);
-	return min(node.Val, childMin);
-	
-}
-
-func min(a int, b int) int{
-	if a > b{
-		return b;
-	}
-	return a;
-}
-
-type TreeNode struct {
-	Val int
-	Left *TreeNode
-	Right *TreeNode
+    var res int
+    bstSmallest(root, &k, &res)
+    return res
 }
