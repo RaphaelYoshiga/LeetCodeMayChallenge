5fca36ef711d22449d38f844e7427b1397938dbf
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-05-22 21:07:11 +0100

    Frequency sort

---
+++ week4/frequencySort.go
@@ -0,0 +1,41 @@
+package main;
+
+import "sort";
+
+type kv struct {
+	Key   rune
+	Value int
+}
+
+func frequencySort(s string) string {
+	hashMap := make(map[rune]int);
+    for _, c := range s{
+		hashMap[c]++;
+	}
+
+    var ss []kv
+    for k, v := range hashMap {
+        ss = append(ss, kv{Key:k, Value:v})
+	}
+	
+	
+    sort.Slice(ss, func(i, j int) bool {
+        return ss[i].Value > ss[j].Value
+	});
+
+	result := make([]rune, len(s));
+	i := 0;
+	for _, kv := range ss{
+		for kv.Value > 0{
+			result[i] = kv.Key;
+			i++;
+			kv.Value--;
+		}
+	}
+
+	return string(result);
+}
+
+func main(){
+	print(frequencySort("tree"));
+}
